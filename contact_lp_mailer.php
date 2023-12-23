d624751e6f446963923cb2877a0e6bbc526aa003
RenzoOrtiz <renzoortizlbk@gmail.com>
2023-12-22 22:23:10 -0300

    send mail contact lp_mail

--- contact_lp_mailer.php
+++ contact_lp_mailer.php
@@ -17,16 +17,15 @@ function createMail($sender, $message)
     $mail->isSMTP(); // protocol that will be used for sending
     $mail->Host = 'smtp.gmail.com'; // Set SMTP server
     $mail->SMTPAuth = true;
-    $mail->Username = 'lavalleies9024@gmail.com'; // SMTP user Gmail account
-    $mail->Password = 'cdbv xmcu rxyw odfr';
+    $mail->Username = 'Businessonmovenoreply@gmail.com'; // SMTP user Gmail account
+    $mail->Password = 'pbin dfqk tvkt cnje';
 
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
     // Recipients and sending
     $mail->setFrom($sender, 'Bussines On The Move');
-    // $mail->addAddress('lavalleies9024@gmail.com', 'Troll'); // Receiver Email, name is optional
-    $mail->addReplyTo('lavalleies9024@gmail.com', 'Reply Information');
+    $mail->addReplyTo('info@businessonmove.com', 'Reply Information');
 
     // Attachments  
 
@@ -35,25 +34,52 @@ function createMail($sender, $message)
     // Content
     $mail->isHTML(true);
     $mail->Subject = 'Customer Support';
-    $mail->Body = $message;
+    #$mail->Body = $message;
+
+    $mail->Body = `<div style="width:500px; min-height:400px; margin:0 auto; background-color:#112724; color:white; font-family:Roboto,RobotoDraft,Helvetica,Arial,sans-serif; padding:40px">
+    <img height="32" src="https://cdn.discordapp.com/attachments/695395214264762433/882325678685556756/g.png" alt="GeoBox">
+        <div style="text-align:center; margin-top: 20px;">
+        <div style="font-weight:bold; font-size:24px; margin-bottom: 10px; margin-top: 50px;">`+ "Hola " + usuariosParaEnviar[cantidad].NombreUsuario + `</div>
+            <div style="font-size:20px;">Hay nuevos cambios en el Campo: `+ campo.NombreCampo + `</div>
+            <div style="font-size:20px;">`+ newMessage + `</div>
+            <button style="background-color: #2a9d8f; color: #f2f2f2; padding: 10px 50px; border: none; font-weight: bold; font-size: 20px; text-decoration: none; font-weight: bold; border-radius: 5px; cursor: pointer; word-break: break-word; margin-top: 60px;">
+                Ver en GeoBox
+            </button>
+        </div>
+    </div>`
 
     return $mail;
 }
 
 
 
-function makeMessage(string $nameUsr)
+function makeMessageUser(string $nameUsr)
 {
-    $message = "Hi $nameUsr,
+    $message = "This user $nameUsr was contact us
 
-  Thank you for contacting Business On The Move. We have received your email and a member of our support team will contact you shortly. We value your time and appreciate your interest in our services, and understand you have specific questions and requirements. That's why we pride ourselves on providing an immediate response. Within the next few hours, one of our representatives will discuss your needs in depth to provide the necessary advice on our products and services. We hope to support you in meeting your business goals and establishing a valuable relationship for both parties. We look forward to speaking with you! Thank you again for your interest in Business On The Move!
+  Thank you for contacting Business On Move. We value your time and appreciate your interest in our services. 
+  Within the next few hours, one of our representatives will discuss your needs in depth to provide the necessary advice on our products and services. 
   
-  Best regards,
+  Thank you for your interest for us!
   
-  [rep name]
-  Business On The Move";
+  Regards,
+  
+  Juan Cruz Rios
+  Business On Move";
     return $message;
 }
+
+
+function makeMessageSupportBOM(string $nameUsr, string $usr_email, string $usr_phone)
+{
+    $message = "This user $nameUsr wants to contact us through the website. Here are their details:
+
+    Phone: $usr_phone
+    Email: $usr_email";
+    return $message;
+}
+
+
 function sendEmail($mail, $addressee)
 {
     try {
@@ -68,9 +94,9 @@ function sendEmail($mail, $addressee)
 //take data
 $fullname = $_POST['fullname'];
 $usr_email = $_POST['email'];
+$usr_phone = $_POST['phone'];
 
-
-$contactEmail = createMail("lavalleies9024@gmail.com", makeMessage($fullname));
+$contactEmail = createMail("Businessonmovenoreply@gmail.com", makeMessageUser($fullname));
 
 if (filter_var($usr_email, FILTER_VALIDATE_EMAIL)) {
 
