c90c1c8fb1e7628ffa0ac839008e70f7ee1a770a
RenzoOrtiz <renzoortizlbk@gmail.com>
2024-01-03 20:23:26 -0300

    HTML Mail

--- contact_lp_mailer.php
+++ contact_lp_mailer.php
@@ -39,45 +39,45 @@ function createMail($sender, $message)
     return $mail;
 }
 
+
 function makeMessageUser(string $nameUsr)
 {
-    $message = "<p>Hi <b><i>$nameUsr</i></b>!</p> 
-
-    <p>Thank you for contacting <u>Business On Move</u>. We value your time and appreciate your interest in our services.</p>
-    
-    <p>Within the next few hours, one of our representatives will discuss your needs in depth to provide the necessary advice on our products and services.</p>
-    
-    <p>Thank you for your interest for us!</p>
-    
-    <p>Regards</p>
-    
-    <p><b>Juan Cruz Rios</b><br>
-    <i>Business On Move</i></p>";
+    $message = "<div style='width:800px; min-height:400px; margin:0 auto; background-color:#205283; color:white; font-family:Roboto,RobotoDraft,Helvetica,Arial,sans-serif; padding:40px'>
+    <img height='100' src='https://cdn.discordapp.com/attachments/482361276337750036/1191878762871140362/logo-bom.jpg?ex=65a70a99&is=65949599&hm=f0f68b38a28f336d57bba7a1fcb9e7516bca598a512bd15c4c574f187385ed2f&' alt='Bom'>
+        <div style='text-align:center; margin-top: 20px;'>
+        <div style='font-weight:bold; font-size:24px; margin-bottom: 10px; margin-top: 50px;'>Hi $nameUsr </div>
+            <div style='font-size:20px;'>Thank you for contacting <u>Business On Move</u>. We value your time and appreciate your interest in our services.</div>
+            <p></p>
+            <div style='font-size:20px;'>Within the next few hours, one of our representatives will discuss your needs in depth to provide the necessary advice on our products and services.</div>
+            <p></p>
+            <div style='font-size:20px;'>Thank you for your interest for us!</div>
+            <p></p>
+            <div style='font-size:20px;'>Regards</div>
+            <a href='http://businessonmove.com' target='_blank'>
+                <button style='background-color: #4499eb; color: #f2f2f2; padding: 10px 50px; border: none; font-weight: bold; font-size: 20px; text-decoration: none; font-weight: bold; border-radius: 5px; cursor: pointer; word-break: break-word; margin-top: 60px;'>
+                    Business On Move
+                </button>
+            </a>
+        </div>
+    </div>";
     return $message;
 }
 
 
-
-
-
-
-
-function makeMessageSupportBOM(string $nameUsr, string $usr_email)
+function makeMessageSupportBOM(string $nameUsr, string $usr_email, string $usr_phone)
 {
-    $message =
-        "<p>This user <span style='color: blue;'><b>$nameUsr</b></span> wants to contact us through the website. Here are their details:</p>
-
-    <p>Email: <a href='mailto:$usr_email' style='color: green;'><u>$usr_email</u></a></p>";
-
+    $message = "<div style='width:800px; min-height:200px; margin:0 auto; background-color:#205283; color:white; font-family:Roboto,RobotoDraft,Helvetica,Arial,sans-serif; padding:40px'>
+    <img height='100' src='https://cdn.discordapp.com/attachments/482361276337750036/1191878762871140362/logo-bom.jpg?ex=65a70a99&is=65949599&hm=f0f68b38a28f336d57bba7a1fcb9e7516bca598a512bd15c4c574f187385ed2f&' alt='Bom'>
+        <div style='text-align:center; margin-top: 20px;'>
+            <div style='font-size:20px;'>This user $nameUsr wants to contact us through the website. Here are their details:</div>
+            <p></p>
+            <div style='font-size:20px;'>Email: $usr_email</div>
+            <p></p>
+            <div style='font-size:20px;'>Phone: $usr_phone</div>
+        </div>
+    </div>";
     return $message;
 }
-
-
-
-
-
-
-
 
 
 function sendEmail($mail, $addressee)
@@ -94,16 +94,16 @@ function sendEmail($mail, $addressee)
 //take data
 $fullname = $_POST['fullname'];
 $usr_email = $_POST['email'];
-
+$usr_phone = $_POST['phone'];
 
 $contactEmailUser = createMail('businessonmovenoreply@gmail.com', makeMessageUser($fullname));
-$contactEmailSupportBOM = createMail('businessonmovenoreply@gmail.com', makeMessageSupportBOM($fullname, $usr_email));
+$contactEmailSupportBOM = createMail('businessonmovenoreply@gmail.com', makeMessageSupportBOM($fullname, $usr_email, $usr_phone));
 
 
 if (filter_var($usr_email, FILTER_VALIDATE_EMAIL)) {
 
     sendEmail($contactEmailUser, $usr_email);
-    sendEmail($contactEmailSupportBOM, 'info@business.com');
+    sendEmail($contactEmailSupportBOM, $usr_email/*'info@business.com'*/);
 
     exit();
 } else {
